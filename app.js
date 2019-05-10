800b57fcab835f6072663896e192c0d3e9d57d04
Minh Pham <mpham@naver.com>
2019-05-10 17:58:51 +0200

    [BACK-END] Make mongoose connection to mongoDB

--- app.js
+++ app.js
@@ -1,5 +1,11 @@
 const express = require("express");
 
+const mongoose = require("mongoose");
+
+const keys = require("./config/keys");
+
+mongoose.connect(keys.mongoURI);
+
 const app = express();
 
 const PORT = process.env.PORT || 5000;

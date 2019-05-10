9c90aa5d2c254dc46543efc086cddf3a9b86b73d
Minh Pham <mpham@naver.com>
2019-05-10 14:22:32 +0200

    [BACK-END] Add Dynamic Port Binding

--- app.js
+++ app.js
@@ -2,10 +2,10 @@ const express = require("express");
 
 const app = express();
 
-const port = 5000;
+const PORT = process.env.PORT || 5000;
 
 app.get("/", (req, res) => {
   res.send("Hello World");
 });
 
-app.listen(port, () => console.log(`App is running on port: ${port}`));
+app.listen(PORT, () => console.log(`App is running on port: ${PORT}`));
